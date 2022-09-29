56580a0daf8a8d55fb71c82e1e41cf39a4138d47
buerbaumer <44548809+buerbaumer@users.noreply.github.com>
2022-09-29 16:37:22 +0200

    Update Mol1_test_V1.py

--- Mol1_test_V1.py
+++ Mol1_test_V1.py
@@ -11,8 +11,20 @@ from rdkit.Chem import AllChem
 
 display_on = False
 
+# sidebar:
+style_choosen = st.sidebar.selectbox('style',['stick','sphere','cartoon','clicksphere', 'line'])
+spin = st.sidebar.checkbox('Spin', value = True)
+color_b = st.sidebar.color_picker('Pick Background color', '#ffffff')
+st.sidebar.write('The current color is', color_b)
+
+# Main page:
 st.title('Analyser for Chemical Structures (ACS)')
 st.write('using py3Dmol, stmol, rdkit, streamlit and data from the National Cancer Institute (https://www.cancer.gov/) ')
+#compound_smiles = st.text_input('Input SMILES','COc1ccc2[nH]c([S@@+]([O-])Cc3ncc(C)c(OC)c3C)nc2c1')
+#compound_input = st.sidebar.selectbox('Input the name of chemical structure: ',['3-Methylheptane', 'Aspirin', 'Diethylsulfate', 'Diethyl sulfate', '50-78-2', 'Adamant'])
+compound_input = st.text_input('Input the name of chemical structure:','Aspirin')
+
+col1, col2 = st.columns(2)
 
 def makeblock(smi):
     mol = Chem.MolFromSmiles(smi)
@@ -33,42 +45,36 @@ def CIRconvert(ids):
         
     
 def render_mol(xyz):
-    xyzview = py3Dmol.view()#(width=600,height=400)
-    xyzview.addModel(xyz,'mol')
-    # xyzview.setStyle({style_choosen: {'radius': 0.1}, 'sphere': {'scale': 0.25}})
-    xyzview.setStyle({style_choosen: {}})
-    # xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
-    xyzview.setBackgroundColor(color_b)
-    #xyzview.setBackgroundColor('white')
+    with col2:
+        xyzview = py3Dmol.view()#(width=600,height=400)
+        xyzview.addModel(xyz,'mol')
+        # xyzview.setStyle({style_choosen: {'radius': 0.1}, 'sphere': {'scale': 0.25}})
+        xyzview.setStyle({style_choosen: {}})
+        # xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
+        xyzview.setBackgroundColor(color_b)
+        #xyzview.setBackgroundColor('white')
     
-    if spin:
-        xyzview.spin(True)
-    else:
-        xyzview.spin(False)
+        if spin:
+            xyzview.spin(True)
+        else:
+            xyzview.spin(False)
         
-    xyzview.zoomTo()
-    showmol(xyzview,height=500,width=500)
+        xyzview.zoomTo()
+        showmol(xyzview,height=500,width=500)
 
-#compound_smiles = st.text_input('Input SMILES','COc1ccc2[nH]c([S@@+]([O-])Cc3ncc(C)c(OC)c3C)nc2c1')
-#compound_input = st.sidebar.selectbox('Input the name of chemical structure: ',['3-Methylheptane', 'Aspirin', 'Diethylsulfate', 'Diethyl sulfate', '50-78-2', 'Adamant'])
-compound_input = st.text_input('Input the name of chemical structure:','Aspirin')
+
 compound_smiles = CIRconvert(compound_input)
 
-#col1, col2 = st.columns(2)
-#co1.st.write("1")
-#co2.st.write("2")
-st.write(compound_smiles)
+with col1:
+    st.write("1")
+    st.write(compound_smiles)
+    
+with col2:
+    st.write("2")
 
-style_choosen = st.sidebar.selectbox('style',['stick','sphere','cartoon','clicksphere', 'line'])
-spin = st.sidebar.checkbox('Spin', value = True)
-color_b = st.sidebar.color_picker('Pick Background color', '#ffffff')
-st.sidebar.write('The current color is', color_b)
-
-# st.write(display_on)
-
-if "Sorry" not in compound_smiles:
-    blk=makeblock(compound_smiles)
-    render_mol(blk)
+    if "Sorry" not in compound_smiles:
+        blk=makeblock(compound_smiles)
+        render_mol(blk)
 
 
 # ---------------------------------------
