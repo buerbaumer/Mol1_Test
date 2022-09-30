b7f90122d6e83082eec0eed09cd9d8ff200c020a
buerbaumer <44548809+buerbaumer@users.noreply.github.com>
2022-09-30 18:36:57 +0200

    Update Mol1_test_V1.py

--- Mol1_test_V1.py
+++ Mol1_test_V1.py
@@ -22,9 +22,8 @@ st.title('Analyser for Chemical Structures (ACS)')
 st.write('using py3Dmol, stmol, rdkit, streamlit and data from the National Cancer Institute (https://www.cancer.gov/) ')
 #compound_smiles = st.text_input('Input SMILES','COc1ccc2[nH]c([S@@+]([O-])Cc3ncc(C)c(OC)c3C)nc2c1')
 #compound_input = st.sidebar.selectbox('Input the name of chemical structure: ',['3-Methylheptane', 'Aspirin', 'Diethylsulfate', 'Diethyl sulfate', '50-78-2', 'Adamant'])
-compound_input = st.text_input('Input the name of chemical structure:','Aspirin')
 
-col1, col2 = st.columns([1,4])
+col1, col2 = st.columns([1,3])
 
 def makeblock(smi):
     mol = Chem.MolFromSmiles(smi)
@@ -77,6 +76,8 @@ compound_rings = CIRconvert_Ring(compound_input)
 
 with col1:
     #st.write("1")
+    compound_input = st.text_input('Input the name of chemical structure:','Aspirin')
+    st.write('')
     st.markdown('**SMILES:**')
     st.write(compound_smiles)
     st.write('')
