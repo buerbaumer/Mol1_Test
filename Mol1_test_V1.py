a7020c41d5e6e6c02e98c89a1674c771d66618d6
buerbaumer <44548809+buerbaumer@users.noreply.github.com>
2022-10-05 07:29:50 +0200

    Update Mol1_test_V1.py

--- Mol1_test_V1.py
+++ Mol1_test_V1.py
@@ -73,7 +73,7 @@ def render_mol(xyz):
         xyzview.addModel(xyz,'mol')
                                               
         if style_choosen == 'balls & stick':
-            xyzview.setStyle({'stick': {'radius': 0.2}, 'sphere': {'scale': 0.25}})
+            xyzview.setStyle({'stick': {'radius': 0.15}, 'sphere': {'scale': 0.25}})
         else:
             xyzview.setStyle({style_choosen: {}})
                                               
